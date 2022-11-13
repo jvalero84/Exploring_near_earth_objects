b85cbc536a23eac90fe61ee8b6f49197fec8b094
jvalero <jvalero84@gmail.com>
2022-11-13 22:48:54 +0100

    Completion of task 4: writting results to csv and json files

--- workspace/models.py
+++ workspace/models.py
@@ -75,6 +75,14 @@ class NearEarthObject:
         """Return `repr(self)`, a computer-readable string representation of this object."""
         return (f"NearEarthObject(designation={self.designation!r}, name={self.name!r}, "
                 f"diameter={self.diameter:.3f}, hazardous={self.hazardous!r})")
+    
+    def serialize(self):
+        return {
+            "designation": self.designation,
+            "name": self.name if self.name is not None else '',
+            "diameter_km": self.diameter,
+            "potentially_hazardous": self.hazardous
+        } 
 
 
 class CloseApproach:
@@ -144,3 +152,11 @@ class CloseApproach:
         """Return `repr(self)`, a computer-readable string representation of this object."""
         return (f"CloseApproach(time={self.time_str!r}, distance={self.distance:.2f}, "
                 f"velocity={self.velocity:.2f}, neo={self.neo!r})")
+    
+    def serialize(self):
+        return {
+            "datetime_utc": self.time_str,
+            "distance_au": self.distance, 
+            "velocity_km_s": self.velocity,
+            "neo": self.neo.serialize()
+        }
--- workspace/write.py
+++ workspace/write.py
@@ -25,8 +25,19 @@ def write_to_csv(results, filename):
     :param filename: A Path-like object pointing to where the data should be saved.
     """
     fieldnames = ('datetime_utc', 'distance_au', 'velocity_km_s', 'designation', 'name', 'diameter_km', 'potentially_hazardous')
-    # TODO: Write the results to a CSV file, following the specification in the instructions.
-
+    # DONE: Write the results to a CSV file, following the specification in the instructions.
+    try:
+        with open(filename, 'w') as outfile:
+            fwritter = csv.writer(outfile)
+            fwritter.writerow(fieldnames)
+            for record in results:
+                fwritter.writerow([record.time, record.distance, record.velocity, record._designation, 
+                record.neo.name if record.neo.name is not None else '', 
+                record.neo.diameter, record.neo.hazardous])
+    except Exception as ex:
+        print(ex)
+    finally:
+        outfile.close
 
 def write_to_json(results, filename):
     """Write an iterable of `CloseApproach` objects to a JSON file.
@@ -39,4 +50,14 @@ def write_to_json(results, filename):
     :param results: An iterable of `CloseApproach` objects.
     :param filename: A Path-like object pointing to where the data should be saved.
     """
-    # TODO: Write the results to a JSON file, following the specification in the instructions.
+    # DONE: Write the results to a JSON file, following the specification in the instructions.
+    wrapper = []
+    for result in results:
+        wrapper.append(result.serialize())
+    try:
+        with open(filename, 'w') as outfile:
+            json.dump(wrapper, outfile, indent=2)
+    except Exception as ex:
+        print(ex)
+    finally:
+        outfile.close
